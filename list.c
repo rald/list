ec292a054bdfe252d836454897b97ee7f3530e3b
rald <rald@users.noreply.github.com>
2016-08-04 01:48:54 +0800

    Add files via upload

--- list.c
+++ list.c
@@ -1,18 +1,72 @@
 #include<stdlib.h>
 #include "list.h"
 
-List_Node *List_NodeNew() {
+List_Node *List_NodeNew(void *data,void *next) {
 	List_Node *node=malloc(sizeof(*node));
 	if(node) {
-		node->data=NULL;
-		node->next=NULL;
+		node->data=data;
+		node->next=next;
 	}
 	return node;
 }
 
-void List_Push(List_Node **head,void *data) {
-  List_Node *node=List_NodeNew();
-  node->data=data;
-  node->next=*head;
-  *head=node;
+List_Node *List_AddFirst(List_Node **head,void *data) {
+	return *head=List_NodeNew(data,*head);
+}
+
+List_Node *List_AddLast(List_Node **head,void *data) {
+	if(!*head) {
+		return List_AddFirst(head,data);
+	} else {
+		List_Node *iter=*head;
+		while(iter->next) iter=iter->next;
+		return iter->next=List_NodeNew(data,NULL);
+	}
+}
+
+List_Node *List_InsertAfter(List_Node **head,void *key,void *data,int(*compareFunction)(void *lhs,void *rhs)) {
+	List_Node *iter=*head;
+	while(iter && compareFunction(iter->data,key)) iter=iter->next;
+	if(iter) return iter->next=List_NodeNew(data,iter->next);
+	return NULL;
+}
+
+List_Node *List_InsertBefore(List_Node **head,void *key,void *data,int(*compareFunction)(void *lhs,void *rhs)) {
+	List_Node *prev;
+	List_Node *curr;
+	if(!*head) return NULL;
+	if(!compareFunction((*head)->data,key)) {
+		return List_AddFirst(head,data);
+	}
+	prev=NULL;
+	curr=*head;
+	while(curr && compareFunction(curr->data,key)) {
+		prev=curr;
+		curr=curr->next;
+	}
+	if(curr) return prev->next=List_NodeNew(data,curr);
+	return NULL;
+}
+
+List_Node *List_Remove(List_Node **head,void *key,int(*compareFunction)(void *lhs,void *rhs)) {
+	List_Node *prev;
+	List_Node *curr;
+	if(!*head) return NULL;
+	if(!compareFunction((*head)->data,key)) {
+		return *head=(*head)->next;
+	}
+	prev=NULL;
+	curr=*head;
+	while(curr && compareFunction(curr->data,key)) {
+		prev=curr;
+		curr=curr->next;
+	}
+	if(!curr) return NULL;
+	prev->next=curr->next;
+	return curr;
+}
+
+void List_Print(List_Node *head,void (*printFunction)(void *data)) {
+	List_Node *iter=head;
+	while(iter) { printFunction(iter->data); iter=iter->next; }
 }
--- main.c
+++ main.c
@@ -3,21 +3,35 @@
 
 #include "list.h"
 
+int compareFunction(void *lhs,void *rhs) {
+	return (*(int*)rhs)-(*(int*)lhs);
+}
+
+void printFunction(void *data) {
+	printf("%d ",*(int*)data);
+}
+
 int main(int argc,char **argv) {
-	List_Node *head=NULL,*iter=NULL;
+	List_Node *head=NULL;
 	int i;
 
 	for(i=0;i<10;i++) {
 		int *j=malloc(sizeof(*j));
 		*j=i;
-		List_Push(&head,(void*)j);
+		List_AddLast(&head,(void*)j);
 	}
+	
+	List_Print(head,printFunction);
+	printf("\n");
 
-	iter=head;
-	while(iter) {
-		printf("%d ",*(int*)iter->data);
-		iter=iter->next ;
-	}
+	int *key=malloc(sizeof(*key));
+	*key=1;
+	if(!List_Remove(&head,key,compareFunction)) {
+		printf("key not found\n");
+	};
+		
+	List_Print(head,printFunction);
+	printf("\n");
 
 	return 0;
 }
